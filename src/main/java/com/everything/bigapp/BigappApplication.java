dd2c515cb6e52eaf702e38fa9a4bba7a76a80a71
Kedar <upskillkedar@gmail.com>
2025-12-16 19:16:06 -0600

    Initial Commit

---
+++ src/main/java/com/everything/bigapp/BigappApplication.java
@@ -0,0 +1,13 @@
+package com.everything.bigapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigappApplication.class, args);
+	}
+
+}
